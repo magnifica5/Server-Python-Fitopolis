0606490f94afd4a25770edb231f9967460c71af5
magnifica5 <tabarcaioana1@gmail.com>
2026-07-23 16:47:03 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -7,7 +7,6 @@ from flask_apscheduler import APScheduler
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
-
 load_dotenv()
 print("am incarcat datele")
 EMAIL_SENDER = os.getenv("EMAIL")
@@ -18,6 +17,7 @@ SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 print("m am conectat ca client supabase")
 ACTIVITIES = ["trezire", "ex1", "masa_dimineata", "masa_pranz", "ex2", "masa_seara", "culcare"]
+
 
 def send_via_smtp(to, subject, message):
     """Trimite email folosind serverul SMTP Gmail"""
@@ -34,6 +34,7 @@ def send_via_smtp(to, subject, message):
     except Exception as e:
         print(f"Eroare SMTP către {to}: {e}")
 
+
 def get_parent_email(parent_id):
     try:
         user_data = supabase.auth.admin.get_user_by_id(parent_id)
@@ -41,6 +42,7 @@ def get_parent_email(parent_id):
     except Exception as e:
         print(f"Eroare Supabase Auth: {e}")
         return None
+
 
 def check_midtime_alerts():
     print(f"[{datetime.now()}] Rulăm check_midtime_alerts...")
@@ -117,21 +119,35 @@ def check_midtime_alerts():
                         "\n- ".join(nerealizate) if nerealizate else "Toate au fost făcute!")
 
                     threading.Thread(target=send_via_smtp, args=(email_parinte, "Raport Zilnic", msg_raport)).start()
+                else:
+                    try:
+                        response = (
+                            supabase.table("children")
+                            .update({"check_verificare": False})
+                            .eq("connection_code", code)
+                            .execute()
+                        )
+                        if response.data:
+                            print("Produs actualizat cu succes:")
+                            print(response.data)
+                        else:
+                            print("Nu s-a găsit niciun produs cu ID-ul specificat sau nu s-au făcut modificări.")
+                            print(response.error)  # Afișează erorile, dacă există
+
+                    except Exception as e:
+                        print(f"A apărut o eroare: {e}")
             except:
                 pass
 
-# 1. Inițializăm obiectul scheduler
 scheduler = APScheduler()
 
-# 2. Adăugăm jobul direct (fără init_app)
 scheduler.add_job(
-    id='check_alerts', 
-    func=check_midtime_alerts, 
-    trigger='interval', 
+    id='check_alerts',
+    func=check_midtime_alerts,
+    trigger='interval',
     minutes=1
 )
 
-# 3. Pornim scheduler-ul
 try:
     scheduler.start()
     print("APScheduler a pornit cu succes în worker.")
@@ -140,5 +156,6 @@ except Exception as e:
 
 # 4. Menținem procesul activ (foarte important pentru Render Background Worker)
 import time
+
 while True:
     time.sleep(1)
